d041bce8e5a98170e0374cff117236a78cc6487c
Colby Patterson <cpatt@byui.edu>
2022-08-19 16:58:15 -0600

    Showing meal details

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,32 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import MealDetails from "../components/MealDetails";
 
-function MealDetailScreen() {
-  return <Text>Meal Detail Screen</Text>;
+import { MEALS } from "../data/dummy-data";
+
+function MealDetailScreen({ route }) {
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default MealDetailScreen;
